1b9b54e27efadbcebbd368317e480f078a8e084b
yanzishang <601260062@qq.com>
2016-08-17 22:04:08 +0800

    connect to mysql

--- dailywalk/models.py
+++ dailywalk/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person (models.Model):
+
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    age = models.IntegerField()
